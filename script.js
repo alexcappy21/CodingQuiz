1cc56979be23ac94acf0d6a388a7af92220a4748
Alexandra Capodilupo <acapodilupo21@aol.com>
2021-01-23 20:35:55 -0500

    added questions and started funaction

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
 
 
+var askQuestion= document.getElementById("#question");
+askQuestion = questions.question[];
 var questions = [
     {
-        question: "question",
-        choices: ["option1", "option2", "option3", "option4"],
-        answer: "option2"
+        question: "Where is the JavaScript placed inside an HTML document or page?",
+        choices: ["In the <body> and <head> sections.", "In the <meta> section.", "In the <title> section.", "In the <footer> section."],
+        answer: "In the <body> and <head> sections."
 
     }, {
-        question: "question",
-        choices: ["option1", "option2", "option3", "option4"],
-        answer: "option2"
+        question: "What kind of statement is used to execute actions based on a trigger or condition?",
+        choices: ["Boolean Variable", "Conditional Statement", "Fired Event", "Regular Expression"],
+        answer: "Conditional Statement"
 
     }, {
-        question: "question",
-        choices: ["option1", "option2", "option3", "option4"],
-        answer: "option2"
+        question: "In JavaScript, what element is used to store multiple values in a single variable?",
+        choices: ["Functions", "Strings", "Variables", "Arrays"],
+        answer: "Arrays"
 
     }, {
-        question: "question",
-        choices: ["option1", "option2", "option3", "option4"],
-        answer: "option2"
+        question: "This is what you call the guide that defines coding conventions for all projects.",
+        choices: ["Coding dictionary", "Style guide", "Main textbook", "Developer's reference"],
+        answer: "Style guide"
 
     }, {
-        question: "question",
-        choices: ["option1", "option2", "option3", "option4"],
-        answer: "option2"
+        question: "What is the name of the object that allows you to perform mathematical tasks with the interpreter?",
+        choices: ["Solve", "Number", "Count", "Math"],
+        answer: "Math"
 
     }
 ]
 
 var questionIndex = 0
 
+
 function checkAnswer(event){
+    document.getElementById("answer-button").click();
     var userChoice = event.target.innerText
     if (userChoice === questions[questionIndex].answer){
         alert("correct");
